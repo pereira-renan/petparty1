b2e106d875f4f307772e1376cc1c52ce1bc2451e
Roberto da Silva Neto <58692973+RobertoDSN@users.noreply.github.com>
2020-10-29 21:24:11 -0300

    Add de paginas

--- TCC/frontend/src/routes.js
+++ TCC/frontend/src/routes.js
@@ -5,11 +5,15 @@ import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastrar";
 import Perfil from "./pages/Profile";
 import EsqueceuSenha from "./pages/EsqueceuSenha";
+import Landing from './pages/Landing';
+import TeacherList from './pages/TeacherList';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/landing" exact component={Landing} />
+        <Route path="/study" component={TeacherList} />
         <Route exact path="/" component={Login} />
         <Route path="/register" component={Cadastro} />
         <Route path="/profile" component={Perfil} />
